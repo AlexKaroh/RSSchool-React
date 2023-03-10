79694b1eb238c6c8549a5ed42e2079d435c9bcab
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-10 17:30:13 +0400

    refactor: add images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import './Header.css';
 import React from 'react';
 import Nav from '../Nav/Nav';
+import reactlogo from '../../assets/reactLogo.svg';
 class Header extends React.Component {
   render() {
     return (
       <header className="header">
+        <img className="header__logo" src={reactlogo} alt="" />
         <Nav />
       </header>
     );
