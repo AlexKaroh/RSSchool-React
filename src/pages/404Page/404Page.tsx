36d96e2e0e8b5a6ebbf489db2460fe96403e9276
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-10 16:04:38 +0400

    feat: migrate 404Page to class component

--- src/pages/404Page/404Page.tsx
+++ src/pages/404Page/404Page.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function NotFound() {
-  return <h1>Not Found</h1>;
+class NotFound extends React.Component {
+  render() {
+    return <h1>Not Found</h1>;
+  }
 }
 
 export default NotFound;
