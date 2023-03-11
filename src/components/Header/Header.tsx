aad00916e33a7e164e97ab5ee16131df57ad2012
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-11 21:20:56 +0400

    feat: add 404 page go home button and css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,14 @@ import './Header.css';
 import React from 'react';
 import Nav from '../Nav/Nav';
 import reactlogo from '../../assets/reactLogo.svg';
+import { Link } from 'react-router-dom';
 class Header extends React.Component {
   render() {
     return (
       <header className="header">
-        <a href="/">
+        <Link to="/">
           <img className="header__logo" src={reactlogo} alt="react logo" />
-        </a>
+        </Link>
         <Nav />
       </header>
     );
--- src/pages/404Page/404Page.tsx
+++ src/pages/404Page/404Page.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import './404Page.css';
+import tinerGif from '../../assets/tinker.gif';
+import { Link } from 'react-router-dom';
 
 class NotFound extends React.Component {
   render() {
-    return <h1>Not Found</h1>;
+    return (
+      <div className="Page404">
+        <div className="video__container">
+          <video autoPlay muted preload="auto" loop className="video">
+            <source
+              type="video/mp4"
+              src="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/homepage/dota_montage_02.mp4"
+            />
+          </video>
+          <div className="video__info">
+            <h1 className="video__header_text">404</h1>
+            <h2 className="video__text">Page not found</h2>
+            <img className="video__image" src={tinerGif} alt="" />
+            <Link className="video__link" to="/">
+              <button className="video__button">Go Home</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
