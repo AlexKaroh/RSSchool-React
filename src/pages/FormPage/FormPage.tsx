e60ff6f411cc8f96b12ae5f72c2ba01cd0bf02aa
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-16 11:03:11 +0400

    feat: create Form page files

---
+++ src/pages/FormPage/FormPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FormPage.css';
+
+class FormPage extends React.Component {
+  render() {
+    return (
+      <div className="FormPage">
+        <form action="">
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default FormPage;
