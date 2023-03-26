c38dbf9d3c49733afd1f9629bcc1ec1296c0688c
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-26 18:32:35 +0400

    feat: add more tests for Header component

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 
 describe('Header component', () => {
@@ -11,5 +11,30 @@ describe('Header component', () => {
       </MemoryRouter>
     );
     expect(screen.getByAltText('dota logo')).toBeInTheDocument();
+  });
+
+  it('render home page', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Header />
+      </MemoryRouter>
+    );
+
+    const pageTitle = getByText('Home');
+    expect(pageTitle).toBeInTheDocument();
+  });
+
+  it('render header', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/heroes']}>
+        <Header />
+        <Routes>
+          <Route path="/heroes"></Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const pageTitle = getByText('Create Hero');
+    expect(pageTitle).toBeInTheDocument();
   });
 });
