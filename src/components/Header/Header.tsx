8fb6511431baa01b0aa92bef150392d49619787f
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-26 17:42:44 +0400

    feat: migrate Header.tsx to function component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,24 +2,22 @@ import './Header.css';
 import React from 'react';
 import Nav from '../Nav/Nav';
 import dotalogo from '../../assets/dotalogo.png';
-import { Link } from 'react-router-dom';
-import { WithRouterProps, withRouter } from '../../withRouter';
-class Header extends React.Component<WithRouterProps> {
-  render() {
-    const { location } = this.props;
-    const pageTitle = location.pathname.substring(1);
-    return (
-      <header className="header">
-        <div className="header__container">
-          <Link to="/">
-            <img className="header__logo" src={dotalogo} alt="dota logo" />
-          </Link>
-          <h1 className="header__page">{pageTitle === '' ? 'Home' : pageTitle}</h1>
-        </div>
-        <Nav />
-      </header>
-    );
-  }
+import { Link, useLocation } from 'react-router-dom';
+
+function Header() {
+  const location = useLocation();
+  const pageTitle = location.pathname.substring(1);
+  return (
+    <header className="header">
+      <div className="header__container">
+        <Link to="/">
+          <img className="header__logo" src={dotalogo} alt="dota logo" />
+        </Link>
+        <h1 className="header__page">{pageTitle === '' ? 'Home' : pageTitle}</h1>
+      </div>
+      <Nav />
+    </header>
+  );
 }
 
-export default withRouter(Header);
+export default Header;
