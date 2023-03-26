83661025a4be4856d25a77f6be46c46532b82d0d
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-26 16:45:38 +0400

    feat: migrate AboutUs.tsx to function component

--- src/pages/AboutUsPage/AboutUs.tsx
+++ src/pages/AboutUsPage/AboutUs.tsx
@@ -4,30 +4,30 @@ import alexkaroh from '../../assets/alexkaroh.jpg';
 import gitlogo from '../../assets/gitLogo.svg';
 import tglogo from '../../assets/tgLogo.svg';
 import linklogo from '../../assets/linkLogo.svg';
-class AboutUs extends React.Component {
-  render() {
-    return (
-      <div className="about">
-        <h1>About me</h1>
-        <div className="about__bio">
-          <h3>
-            Hello , my name is Alex and nowdays I`m study on React course by Rolling Scopes School
-          </h3>
-        </div>
-        <img className="about__photo" src={alexkaroh} alt="author" />
-        <div className="about__social">
-          <a href="https://github.com/alexkaroh/">
-            <img className="about__item" src={gitlogo} alt="git logo" />
-          </a>
-          <a href="https://linkedin.com/in/alexey-poklad-974735253/">
-            <img className="about__item" src={linklogo} alt="linkedin logo" />
-          </a>
-          <a href="https://t.me/hloyahustle">
-            <img className="about__tg about__item" src={tglogo} alt="telegram logo" />
-          </a>
-        </div>
+
+function AboutUs() {
+  return (
+    <div className="about">
+      <h1>About me</h1>
+      <div className="about__bio">
+        <h3>
+          Hello , my name is Alex and nowdays I`m study on React course by Rolling Scopes School
+        </h3>
       </div>
-    );
-  }
+      <img className="about__photo" src={alexkaroh} alt="author" />
+      <div className="about__social">
+        <a href="https://github.com/alexkaroh/">
+          <img className="about__item" src={gitlogo} alt="git logo" />
+        </a>
+        <a href="https://linkedin.com/in/alexey-poklad-974735253/">
+          <img className="about__item" src={linklogo} alt="linkedin logo" />
+        </a>
+        <a href="https://t.me/hloyahustle">
+          <img className="about__tg about__item" src={tglogo} alt="telegram logo" />
+        </a>
+      </div>
+    </div>
+  );
 }
+
 export default AboutUs;
