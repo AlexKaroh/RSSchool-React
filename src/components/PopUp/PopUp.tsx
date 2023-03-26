7e4b8010d91385e0754be2ce0aad44f2d0e32aeb
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-26 16:47:04 +0400

    feat: migrate PopUp.tsx to function component

--- src/components/PopUp/PopUp.tsx
+++ src/components/PopUp/PopUp.tsx
@@ -1,14 +1,12 @@
 import './PopUp.css';
 import React from 'react';
 
-class PopUp extends React.Component {
-  render() {
-    return (
-      <div className="popup">
-        <h4 className="popup__message green">Confirmed! the data has been saved.</h4>
-      </div>
-    );
-  }
+function PopUp() {
+  return (
+    <div className="popup">
+      <h4 className="popup__message green">Confirmed! the data has been saved.</h4>
+    </div>
+  );
 }
 
 export default PopUp;
