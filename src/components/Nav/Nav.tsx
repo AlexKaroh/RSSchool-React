7104b6d4d4dfbaa56dd66f9c0640fb1c11f79995
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-26 16:47:47 +0400

    feat: migrate Nav.tsx to function component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,30 +2,28 @@ import './Nav.css';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-class Nav extends React.Component {
-  render() {
-    return (
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="nav__item">
-            <NavLink className="nav__link" to="/">
-              Home Page
-            </NavLink>
-          </li>
-          <li className="nav__item">
-            <NavLink className="nav__link" to="/AboutUs">
-              About Us
-            </NavLink>
-          </li>
-          <li className="nav__item">
-            <NavLink className="nav__link" to="/Form">
-              Create Hero
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+function Nav() {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <NavLink className="nav__link" to="/">
+            Home Page
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink className="nav__link" to="/AboutUs">
+            About Us
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink className="nav__link" to="/Form">
+            Create Hero
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
