d37a6c76ab8d706245d338d4ceb8c61ab911ef99
AlexKaroh <steam.alexkaroh@gmail.com>
2023-03-26 17:52:21 +0400

    feat: migrate CustomCard.tsx to function component

--- src/components/CustomCard/CustomCard.tsx
+++ src/components/CustomCard/CustomCard.tsx
@@ -2,9 +2,16 @@ import './CustomCard.css';
 import ICustomCardProps from '../../interfaces/ICustomCardProps';
 import React from 'react';
 
-class CustomCard extends React.Component<ICustomCardProps> {
-  createHeroAttribute = () => {
-    switch (this.props.heroAttribute) {
+const CustomCard: React.FC<ICustomCardProps> = ({
+  heroName,
+  heroImage,
+  heroRole,
+  heroDate,
+  heroAttribute,
+  heroTypeAttack,
+}) => {
+  const createHeroAttribute = () => {
+    switch (heroAttribute) {
       case 'Agility':
         return 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png';
       case 'Strength':
@@ -16,8 +23,8 @@ class CustomCard extends React.Component<ICustomCardProps> {
     }
   };
 
-  createHeroTypeAttack = () => {
-    switch (this.props.heroTypeAttack) {
+  const createHeroTypeAttack = () => {
+    switch (heroTypeAttack) {
       case false:
         return 'Melee';
       case true:
@@ -27,27 +34,24 @@ class CustomCard extends React.Component<ICustomCardProps> {
     }
   };
 
-  render() {
-    const { heroName, heroImage, heroRole, heroDate } = this.props;
-    return (
-      <div className="card">
-        <div className="card__image_container">
-          <img src={heroImage} className="card__image" alt="hero image" />
-        </div>
-        <div className="card__hero_name">{heroName}</div>
-        <div className="card__image_header">
-          <div className="card__hero_attack">{this.createHeroTypeAttack()}</div>
-          <img
-            src={this.createHeroAttribute()}
-            className="card__hero_attribute_image"
-            alt="hero_attribute"
-          />
-          <div className="card__hero_role">{heroRole}</div>
-        </div>
-        <div className="card__hero_date">Release date: {heroDate}</div>
+  return (
+    <div className="card">
+      <div className="card__image_container">
+        <img src={heroImage} className="card__image" alt="hero image" />
       </div>
-    );
-  }
-}
+      <div className="card__hero_name">{heroName}</div>
+      <div className="card__image_header">
+        <div className="card__hero_attack">{createHeroTypeAttack()}</div>
+        <img
+          src={createHeroAttribute()}
+          className="card__hero_attribute_image"
+          alt="hero_attribute"
+        />
+        <div className="card__hero_role">{heroRole}</div>
+      </div>
+      <div className="card__hero_date">Release date: {heroDate}</div>
+    </div>
+  );
+};
 
 export default CustomCard;
