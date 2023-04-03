72684cd091d853e5a259824e19b1abe8db00f0ca
AlexKaroh <steam.alexkaroh@gmail.com>
2023-04-03 13:47:10 +0400

    refactor: delete unnecessary files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({
-    plugins: [react()],
-});
